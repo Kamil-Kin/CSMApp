059b09d7c3b32358a4359e428ab66ad83c0cbc02
Kamil-Kin <kacper.kin28@gmail.com>
2017-07-10 22:46:32 +0200

    Zdarzenie & proces

--- CSMApp/pakiet.h
+++ CSMApp/pakiet.h
@@ -1,14 +1,21 @@
 #ifndef CSMA_PP_PAKIET_H
 #define CSMA_PP_PAKIET_H
 
+#include "zdarzenie.h"
+
 class Pakiet 
 {
 public:
-  Pakiet(int idx);
+  Pakiet(int idx) :faza_(0), skonczony_(false) { moje_zd_ = new Zdarzenie(this); }
   ~Pakiet();
+  void execute();
+  void aktywacja(double czas);
+  int faza_;
+  bool skonczony_;
   int getId();
 private:
   int id_tx_;
+  Zdarzenie* moje_zd_;
 };
 
 #endif // !CSMA_PP_PAKIET_H
--- CSMApp/zdarzenie.h
+++ CSMApp/zdarzenie.h
@@ -1,9 +1,17 @@
 #ifndef CSMA_PP_ZDARZENIE_H
 #define CSMA_PP_ZDARZENIE_H
 
+class Pakiet;
+
 class Zdarzenie 
 {
-
+public:
+  Zdarzenie(Pakiet *pak) : czas_zdarzenia_(0.0), pakiet_(pak) {}
+  void ustaw_czas_zd(double time) { czas_zdarzenia_ = time; }
+  double pobierz_czas_zd() { return czas_zdarzenia_; }
+private:
+  double czas_zdarzenia_;
+  Pakiet* pakiet_;
 };
 
 #endif // !CSMA_PP_ZDARZENIE_H

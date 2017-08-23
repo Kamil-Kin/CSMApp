fb4bd7d0314e7bf632fc12b05eb5f48c4b166d62
Kamil-Kin <kacper.kin28@gmail.com>
2017-08-24 00:05:58 +0200

    histogramy wykresy matlab

--- CSMApp/histogramy.m
+++ CSMApp/histogramy.m
@@ -1,4 +1,21 @@
-fileID = fopen('hist_rownomierny.txt','r');
-[R,size] = fscanf(fileID,'%f');
-nbins = 10;
-histogram(R,nbins);
+fileIDr = fopen('hist_rownomierny.txt','r');
+[R,size] = fscanf(fileIDr,'%f');
+nbins = 100;
+figure(1)
+title('Histogram rozk³adu równomiernego');
+xlabel('Losowane liczby');
+ylabel('Iloœæ wylosowanych liczb');
+hist(R,nbins);
+fclose(fileIDr);
+
+fileIDw = fopen('hist_wykladniczy.txt','r');
+[W,size] = fscanf(fileIDw,'%f');
+nbins = 100;
+figure(2)
+title('Histogram rozk³adu wyk³adniczego');
+xlabel('Losowane liczby');
+ylabel('Iloœæ wylosowanych liczb');
+hist(W,nbins);
+fclose(fileIDw);
+
+

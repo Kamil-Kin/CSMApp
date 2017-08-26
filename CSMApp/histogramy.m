ee80c962c1790972d53b42ee4a0204ac7f72ed32
Kamil-Kin <kacper.kin28@gmail.com>
2017-08-26 23:41:14 +0200

    histogramy matlab

--- CSMApp/histogramy.m
+++ CSMApp/histogramy.m
@@ -1,21 +1,26 @@
 fileIDr = fopen('hist_rownomierny.txt','r');
 [R,size] = fscanf(fileIDr,'%f');
 nbins = 100;
+
 figure(1)
+hist(R,nbins);
+grid on;
 title('Histogram rozk³adu równomiernego');
 xlabel('Losowane liczby');
 ylabel('Iloœæ wylosowanych liczb');
-hist(R,nbins);
+
 fclose(fileIDr);
 
 fileIDw = fopen('hist_wykladniczy.txt','r');
 [W,size] = fscanf(fileIDw,'%f');
 nbins = 100;
+
 figure(2)
+hist(W,nbins);
+grid on;
 title('Histogram rozk³adu wyk³adniczego');
 xlabel('Losowane liczby');
 ylabel('Iloœæ wylosowanych liczb');
-hist(W,nbins);
 fclose(fileIDw);
 
 
